87e44adcce32c1925aab6175d19c4eb8b308bf49
Daniyal Ashraf Khan <zkac127@live.rhul.ac.uk>
2023-11-02 01:54:17 +0000

    Restructured file system and added TypeScript code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
 1 represents user pieces
 */
 
-let board_start = [
+let board_start: number[][] = [
     [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
     [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
     [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
@@ -16,21 +16,22 @@ let board_start = [
 ]
 
 // getting all rows from the grid
-const rows = document.querySelectorAll('.board-container .row')
+const rows = document.querySelectorAll('.board-container .row')!;
+
 
 // looping through each of the rows and their individual columns to store the correct value from the array index
 rows.forEach((row, rowIndex) => {
-    const cols = row.querySelectorAll('.col');
+    const cols = row.querySelectorAll('.col')!;
     cols.forEach((col, colIndex) => {
         if (board_start[rowIndex][colIndex] === 1) {
             // 1 to represent user pieces
-            col.innerText = '1'; 
+            col.textContent = '1'; 
         } else if (board_start[rowIndex][colIndex] === -1) {
             // -1 to represent opponent pieces
-            col.innerText = '-1'; 
+            col.textContent = '-1'; 
         } else {
             // '' to represent empty squares
-            col.innerText = ''; 
+            col.textContent = ''; 
         }
     });
 });
