d39a457ef6052c38bcfbb17c045fdb483aba0725
Daniyal Ashraf Khan <zkac127@live.rhul.ac.uk>
2023-11-23 20:07:21 +0000

    Comprehensively tested the board initalization and properties The classes tested here are: CheckersBoard, CheckersPiece and PieceColor Testing for whether the board is correctly initalized, number of pieces and edge cases/out of bound areas on the board

--- src/test/board.test.ts
+++ src/test/board.test.ts
@@ -1,20 +1,81 @@
 import { CheckersBoard, CheckersPiece, PieceColor } from "../checkers";
 
 describe('CheckersBoard', () => {
-    let board: CheckersBoard;
+    let checkersBoard: CheckersBoard;
 
     beforeEach(() => {
-        board = new CheckersBoard();
+        checkersBoard = new CheckersBoard();
     })
 
     test('Board should be correctly initialized', () => {
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
-                if (row < 3 && (row + col % 2 === 1)) {
-                    expect(board.getPiece(row, col)).toBeInstanceOf(CheckersPiece);
-                    expect(board.getPiece(row, col)?.color).toBe(PieceColor.Black);
+                if (row < 3 && (row + col) % 2 === 1) {
+                    expect(checkersBoard.getPiece(row, col)).toBeInstanceOf(CheckersPiece);
+                    expect(checkersBoard.getPiece(row, col)?.color).toBe(PieceColor.Black);
+                    expect(checkersBoard.getPiece(row, col)?.isKing).toBe(false);
+                } else if (row > 4 && (row + col) % 2 === 1) {
+                    expect(checkersBoard.getPiece(row, col)).toBeInstanceOf(CheckersPiece);
+                    expect(checkersBoard.getPiece(row, col)?.color).toBe(PieceColor.Red);
+                    expect(checkersBoard.getPiece(row, col)?.isKing).toBe(false);
+                } else {
+                    expect(checkersBoard.getPiece(row, col)).toBeNull();
                 }
             }
         }
+        expect(checkersBoard.board.length).toBe(8);
+        checkersBoard.board.forEach(row => {
+            expect(row.length).toBe(8);
+        })
+    });
+});
+
+describe('CheckersBoard', () => {
+    let checkersBoard: CheckersBoard;
+
+    beforeEach(() => {
+        checkersBoard = new CheckersBoard();
     })
-})
+
+    test('Ensure number of pieces is 12 and the same for Red and Black', () => {
+        let blackPieces = 0;
+        let redPieces = 0;
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                if (row < 3 && (row + col) % 2 === 1) {
+                    const piece = checkersBoard.getPiece(row, col);
+                    if (piece) {
+                        if (piece.color === PieceColor.Black) {
+                            blackPieces++;
+                        }
+                    }
+                } else if (row > 4 && (row + col) % 2 === 1) {
+                    const piece = checkersBoard.getPiece(row, col);
+                    if (piece) {
+                        if (piece.color === PieceColor.Red) {
+                            redPieces++;
+                        }
+                    }
+                } else {
+                    expect(checkersBoard.getPiece(row, col)).toBeNull();
+                }
+            }
+        }
+        expect(blackPieces).toBe(12);
+        expect(redPieces).toBe(12);
+    });
+});
+
+describe('CheckersBoard', () => {
+    let checkersBoard: CheckersBoard;
+
+    beforeEach(() => {
+        checkersBoard = new CheckersBoard();
+    })
+
+    test('Accessing out of bounds should be handled', () => {
+        expect(checkersBoard.board[10]).toBeUndefined();
+        expect(checkersBoard.board[0][19]).toBeUndefined();
+        expect(checkersBoard.board[-1]).toBeUndefined();
+    });    
+});
