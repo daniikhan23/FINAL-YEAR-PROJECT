9e6886efb7ec3e68637ff6522283a70bf5835ebd
Daniyal Ashraf Khan <zkac127@live.rhul.ac.uk>
2024-03-11 04:13:24 +0000

    Reconfigured jest for testing and removed some unnecessary files Diary update it also included

---
+++ checkersmate/jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest",
+  },
+};
+
+export default config;
--- checkersmate/src/pages/game/Game.tsx
+++ checkersmate/src/pages/game/Game.tsx
@@ -1,67 +1,11 @@
-import * as React from 'react';
-import '../../css/game-styling.css';
-import Navbar from '../../components/navbar/Navbar';
+import * as React from "react";
+import "../../css/game-styling.css";
+import Navbar from "../../components/navbar/Navbar";
 
 const Game = () => {
-  function MyButton() {
-    function handleClick() {
-      alert('You clicked me!');
-    }
-
-    return (
-      <button onClick={handleClick}>
-        Click me!
-      </button>
-    )
-  }
-  
-  const user= {
-    name: 'Hedy Lamar',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-  };
-  
-  const products = [
-    {title: 'Cabbage', isFruit: false, id: 1},
-    {title: 'Garlic', isFruit: false, id: 2},
-    {title: 'Apple', isFruit: false, id: 3},
-  ]
-  
-  const listItems = products.map(product => 
-    <li 
-      key={product.id}
-      style={{
-        color: product.isFruit? 'magenta' :   'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-    );
-
   return (
     <>
-      <Navbar/>
-      <h1>Welcome to my app</h1>
-
-      <h1>Home</h1>
-      <p>hello there <br /> how u doin</p>
-      <h1>
-        {user.name}
-      </h1>
-      <img 
-        className="avatar" 
-        src={user.imageUrl}
-        alt='lol'
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-
-       <ul>{listItems}</ul>
-
-        <MyButton/>
-
+      <Navbar />
     </>
   );
 };
