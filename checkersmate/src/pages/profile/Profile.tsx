78e12a6f3719edbf6be1c6b91fdce0e4236f5465
Daniyal Ashraf Khan <zkac127@live.rhul.ac.uk>
2024-03-16 05:35:11 +0000

    User information is now displayed on the profile page

--- checkersmate/src/pages/profile/Profile.tsx
+++ checkersmate/src/pages/profile/Profile.tsx
@@ -1,13 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { useAuth } from "../../context/UserAuthContext";
 import randomImage from "../../assets/react.svg";
 import "../../css/profile.css";
+import { toast } from "react-toastify";
+import ReactCountryFlag from "react-country-flag";
+
+interface ProfileData {
+  username: string;
+  fullName: string;
+  email: string;
+  country: string;
+}
 
 const Profile = () => {
+  const [profileData, setProfileData] = useState<ProfileData>({
+    username: "",
+    fullName: "",
+    email: "",
+    country: "",
+  });
+  const { currentUser } = useAuth();
+  const db = getFirestore();
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      if (currentUser) {
+        const docRef = doc(db, "users", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setProfileData(docSnap.data() as ProfileData);
+        } else {
+          toast.error(
+            "No profile related data found! Contact support to fix the issue."
+          );
+        }
+      }
+    };
+    fetchProfileData();
+  }, [currentUser, db]);
+
   return (
     <div className="profile">
       <div className="profile-header">
         <img src={randomImage} alt="" />
-        <h5>username</h5>
-        <h5>flag icon</h5>
+        <h5>{profileData.username}</h5>
+        {profileData.country && (
+          <ReactCountryFlag
+            countryCode={profileData.country}
+            svg
+            style={{
+              width: "2em",
+              height: "2em",
+            }}
+            title={profileData.country}
+          />
+        )}
       </div>
       <div className="profile-body">
         <div className="data">
@@ -16,18 +65,16 @@ const Profile = () => {
             <h5>Full Name</h5>
             <h5>Email Address</h5>
             <h5>Country</h5>
-            <h5>Language</h5>
             <h5>Record (W/L/D)</h5>
             <h5>Rating</h5>
           </div>
           <div className="info">
-            <h5>random</h5>
-            <h5>random</h5>
-            <h5>random</h5>
-            <h5>random</h5>
-            <h5>random</h5>
-            <h5>random</h5>
-            <h5>random</h5>
+            <h5>{profileData.username}</h5>
+            <h5>{profileData.fullName}</h5>
+            <h5>{profileData.email}</h5>
+            <h5>{profileData.country}</h5>
+            <h5>Coming Soon</h5>
+            <h5>Coming Soon</h5>
           </div>
         </div>
         <div className="about">
