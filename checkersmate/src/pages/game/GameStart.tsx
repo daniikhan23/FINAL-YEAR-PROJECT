b9bd6204f8f87d05ef0c1229c5b13d99a5155099
Daniyal Ashraf Khan <zkac127@live.rhul.ac.uk>
2024-03-21 02:15:41 +0000

    Player one's name is set on the page after fetching it from firestore db

--- checkersmate/src/pages/game/GameStart.tsx
+++ checkersmate/src/pages/game/GameStart.tsx
@@ -1,18 +1,37 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import "../../css/game-start.css";
 import { useStyle } from "../../context/StyleContext";
 import backgroundImage from "../../assets/img/background.png";
 import redKing from "../../assets/img/redKing.png";
 import blackKing from "../../assets/img/blackKing.png";
+import { useAuth } from "../../context/UserAuthContext";
 
 const GameStart = () => {
+  const [playerOneUser, setPlayerOneUser] = useState("");
+  const [playerTwoUser, setPlayerTwoUser] = useState("");
+  const [gameMode, setGameMode] = useState(false);
+  const { currentUser } = useAuth();
+  const db = getFirestore();
   const { changeBodyBackground } = useStyle();
   useEffect(() => {
+    const fetchPlayer1Username = async () => {
+      if (currentUser) {
+        const userDocRef = doc(db, "users", currentUser.uid);
+        const userDocSnap = await getDoc(userDocRef);
+        if (userDocSnap.exists()) {
+          const userData = userDocSnap.data();
+          setPlayerOneUser(userData.username);
+        }
+      }
+    };
+    fetchPlayer1Username();
     // set background
     changeBodyBackground(backgroundImage);
 
     return () => changeBodyBackground("wheat");
   }, [changeBodyBackground]);
+
   return (
     <>
       <div className="game-start">
@@ -22,11 +41,11 @@ const GameStart = () => {
             <img src={blackKing} alt="" className="blackking" />
           </div>
           <div className="main">
-            <h5>Choose Game Mode:</h5>
+            <h4>Choose Game Mode:</h4>
             <h5>place a dropdown here</h5>
-            <h5 className="player1">Player 1:</h5>
-            <h5>username</h5>
-            <h5 className="player2">Player 2:</h5>
+            <h4 className="player1">Player 1:</h4>
+            <h5>{playerOneUser}</h5>
+            <h4 className="player2">Player 2:</h4>
             <h5>opponent name</h5>
             <button>Local Play</button>
             <button>Play vs AI</button>
